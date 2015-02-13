aadb54a08db9d17861410d5c3a620bc9b173104c
YasotiDev <yasotidev@outlook.fr>
2015-02-13 12:56:19 +0100

    Add User Profile

---
+++ BoomerApi/Src/Server/Core/Boomer.Server.Core.Data/UserProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using Boomer.Server.Core.Contract;
+using Boomer.Server.Core.Model;
+
+namespace Boomer.Server.Core.Data
+{
+    public class UserProfileRepository : EFRepository<UserProfile>, IUserProfileRepository
+    {
+        public UserProfileRepository(DbContext dbContext) 
+            : base(dbContext)
+        {
+        }
+    }
+}
