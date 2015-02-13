b31ef20f0949a61550f9932c38ee19b5e6eb2567
YasotiDev <yasotidev@outlook.fr>
2015-02-13 13:22:12 +0100

    Entity UserLogin

--- BoomerApi/Src/Server/Core/Boomer.Server.Core.Model/UserLogin.cs
+++ BoomerApi/Src/Server/Core/Boomer.Server.Core.Model/UserLogin.cs
@@ -14,5 +14,9 @@ namespace Boomer.Server.Core.Model
         }
 
         public string Id { get; set; }
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
     }
 }
