96a869e0b5c213ce7ae9c75f2dc6210f70cfa18b
YasotiDev <yasotidev@outlook.fr>
2015-02-13 02:28:34 +0100

    Add First Entities Refract

--- BoomerApi/Src/Server/Core/Boomer.Server.Core.Contract/IRoleGroupRepository.cs
+++ BoomerApi/Src/Server/Core/Boomer.Server.Core.Contract/IRoleGroupRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Boomer.Server.Core.Contract
+﻿namespace Boomer.Server.Core.Contract
 {
     public interface IRoleGroupRepository
     {
